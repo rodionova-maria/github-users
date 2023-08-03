ec925f58b83d20f69eb4f9e6305327efc9aa48fd
rodionova-maria <125979012+rodionova-maria@users.noreply.github.com>
2023-08-04 02:31:02 +0300

    init with webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+}

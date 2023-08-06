c148c33fa70ce6eaea95b70f30e32bbcd9aa4c46
rodionova-maria <125979012+rodionova-maria@users.noreply.github.com>
2023-08-07 01:27:57 +0300

    remove token

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,14 +1,12 @@
 import axios from 'axios'
 
 const GITHUB_API_BASE_URL = 'https://api.github.com'
-const GITHUB_ACCESS_TOKEN = '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'
 
-export const PER_PAGE = 20
+export const PER_PAGE = 40
 
 export const axiosInstance = axios.create({
   baseURL: `${GITHUB_API_BASE_URL}`,
   headers: {
-    Authorization: `token ${GITHUB_ACCESS_TOKEN}`,
     Accept: 'application/vnd.github.v3+json'
   }
 })

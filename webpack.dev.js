b8a3e778bbbf02cf4e7d8206a001f84a13794c64
rodionova-maria <125979012+rodionova-maria@users.noreply.github.com>
2023-08-07 03:04:23 +0300

    edit config

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,7 +6,6 @@ module.exports = merge(webpackConfig, {
   entry: './src/index.tsx',
   devServer: {
     compress: true,
-    // public: 'github-users-u18z.onrender.com',
     allowedHosts: 'all',
     historyApiFallback: true
   }
